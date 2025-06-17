67bf38877c1aaf0cf875dc250cb8faf3dfe1ac8a
Mistyttm <contact@mistyttm.dev>
2025-06-17 20:21:12 +1000

    chore: Formatting

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,12 +3,12 @@ let
   lib = prev.lib;
   entries = builtins.readDir ./.;
   dirs = lib.filterAttrs (name: type: type == "directory") entries;
-  
+
   packageDirs = lib.filterAttrs (name: type:
-    type == "directory" && 
+    type == "directory" &&
     builtins.pathExists (./${name} + "/default.nix")
   ) dirs;
-  
+
   packages = lib.mapAttrs (name: _:
     prev.callPackage (./${name}) { }
   ) packageDirs;
