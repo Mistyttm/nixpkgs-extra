94008c41589e4f6368d8146fbfd826370072c520
Mistyttm <contact@mistyttm.dev>
2026-01-21 17:44:26 +1000

    fix: expose modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,13 @@
 {
-  default =
-    { ... }:
-    {
-      imports = [
-        ./tdarr.nix
-        # ./steam-presence.nix
-      ];
-    };
+  # Default: all modules imported together
+  default = { ... }: {
+    imports = [
+      ./tdarr.nix
+      # ./steam-presence.nix
+    ];
+  };
+
+  # Individual module access
+  tdarr = import ./tdarr.nix;
+  # steam-presence = import ./steam-presence.nix;
 }
