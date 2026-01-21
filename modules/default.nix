062d1ea18d24a37aab61083ab801cbe531e28246
Mistyttm <contact@mistyttm.dev>
2026-01-21 17:51:08 +1000

    fix: module importing

--- modules/default.nix
+++ modules/default.nix
@@ -1,13 +1,6 @@
-{
-  # Default: all modules imported together
-  default = { ... }: {
-    imports = [
-      ./tdarr.nix
-      # ./steam-presence.nix
-    ];
-  };
-
-  # Individual module access
-  tdarr = import ./tdarr.nix;
-  # steam-presence = import ./steam-presence.nix;
+{ ... }: {
+  imports = [
+    ./tdarr.nix
+    # ./steam-presence.nix
+  ];
 }
